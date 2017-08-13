b68f7c942fc25f36391e13909c7053d9990f93a8
avikkundu <30687032+avikkundu@users.noreply.github.com>
2017-08-13 11:36:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article one', function (req, res) {
-  res.send(`Article one requested .oh let me give some time.`);
+  res.send('Article one requested .oh let me give some time.');
 });
 app.get('/article two', function (req, res) {
   res.send(`Article two requested .this is the second one.`);
